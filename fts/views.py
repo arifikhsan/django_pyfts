574da998885b9ab3e16e0c0969d288662ae34df2
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-02-06 23:25:58 +0700

    aaa

--- fts/views.py
+++ fts/views.py
@@ -6,19 +6,23 @@ from pyFTS.partitioners import Grid
 from pyFTS.models import chen
 import warnings
 
+
 def index(request):
     warnings.filterwarnings('ignore')
 
     train = Enrollments.get_data()
     test = Enrollments.get_data()
     df = Enrollments.get_dataframe()
+    aa = df['Enrollments'].values.tolist()
 
     fs = Grid.GridPartitioner(data=train, npart=10)
     model = chen.ConventionalFTS(partitioner=fs)
     model.fit(train)
-    # print(model)
-    forecasts = model.predict(test)
+    # print(aa)
+    # forecasts = model.predict(test)
+    forecasts = model.predict([18876])
 
-    data = {'message': 'Hello world', 'forecast': forecasts}
+    # data = {'message': 'Hello world', 'train': aa }
+    data = {'message': 'Hello world', 'train': aa, 'forecast': forecasts}
 
     return JsonResponse(data)
